24e8f9c004b9de2fa4ba6f91293d6c3612159275
iftekher2108 <parvezkhan21082002@gmail.com>
2024-10-20 18:24:26 +0600

    next portfolio

---
+++ pages/about.jsx
@@ -0,0 +1,8 @@
+
+export default function about() {
+    return (
+        <>
+        <div className="text-red-500 m-20 rounded-lg bg-teal-500 p-10">iftekher mahmud pervez</div>
+        </>  
+    )
+}
