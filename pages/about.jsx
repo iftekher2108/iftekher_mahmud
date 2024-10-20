fe1c4e0c4a601cbef784e5c18d3ea221dbe6d9af
iftekher2108 <parvezkhan21082002@gmail.com>
2024-10-20 18:28:22 +0600

    mejor change

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@
 export default function about() {
     return (
         <>
-        <div className="text-red-500 m-20 rounded-lg bg-teal-500 p-10">iftekher mahmud pervez</div>
+        <div className=" m-20 rounded-lg bg-teal-700 p-10">iftekher mahmud pervez</div>
         </>  
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function Index() {
     <Layout>
       <div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-red-400 p-10 rounded-md">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-red-700 p-10 rounded-md">
           <Image
             className="dark:invert"
             src="https://nextjs.org/icons/next.svg"
